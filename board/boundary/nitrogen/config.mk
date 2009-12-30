6ae6e368b9654037ea145186f1ff76e4ec369a81
Eric Nelson <ericn@ericsony.(none)>
2009-12-29 20:38:12 -0700

    Modified boot address

--- board/boundary/nitrogen/config.mk
+++ board/boundary/nitrogen/config.mk
@@ -3,6 +3,6 @@ LDSCRIPT := $(SRCTREE)/board/$(VENDOR)/$(BOARD)/u-boot.lds
 sinclude $(OBJTREE)/board/$(VENDOR)/$(BOARD)/config.tmp
 
 ifndef TEXT_BASE
-	TEXT_BASE = 0x97800000
+	TEXT_BASE = 0x93f00000
 endif
 CONFIG_HW_WATCHDOG=y
