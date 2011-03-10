ddefd51baa27b8b9a69491b5611436f5ca41c029
Troy Kisky <troy.kisky@boundarydevices.com>
2011-03-09 17:33:41 -0700

    mx51: generic.c: rearrange like mx53 generic.c

--- cpu/arm_cortexa8/mx51/generic.c
+++ cpu/arm_cortexa8/mx51/generic.c
@@ -307,6 +307,40 @@ static u32 __get_lp_apm(void)
 	return ret_val;
 }
 
+static u32 __get_uart_clk(void)
+{
+	u32 freq = 0, reg, pred, podf;
+	reg = __REG(MXC_CCM_CSCMR1);
+	switch ((reg & MXC_CCM_CSCMR1_UART_CLK_SEL_MASK) >>
+		MXC_CCM_CSCMR1_UART_CLK_SEL_OFFSET) {
+	case 0x0:
+		freq = __decode_pll(PLL1_CLK, CONFIG_MX51_HCLK_FREQ);
+		break;
+	case 0x1:
+		freq = __decode_pll(PLL2_CLK, CONFIG_MX51_HCLK_FREQ);
+		break;
+	case 0x2:
+		freq = __decode_pll(PLL3_CLK, CONFIG_MX51_HCLK_FREQ);
+		break;
+	case 0x3:
+		freq = __get_lp_apm();
+		break;
+	default:
+		break;
+	}
+
+	reg = __REG(MXC_CCM_CSCDR1);
+
+	pred = (reg & MXC_CCM_CSCDR1_UART_CLK_PRED_MASK) >>
+		MXC_CCM_CSCDR1_UART_CLK_PRED_OFFSET;
+
+	podf = (reg & MXC_CCM_CSCDR1_UART_CLK_PODF_MASK) >>
+		MXC_CCM_CSCDR1_UART_CLK_PODF_OFFSET;
+	freq /= (pred + 1) * (podf + 1);
+
+	return freq;
+}
+
 static u32 __get_pll_from_choice(unsigned choice)
 {
 	u32 freq;
@@ -668,41 +702,6 @@ static u32 calc_per_cbcdr_val(u32 per_clk, u32 cbcmr)
 
 	return cbcdr;
 }
-
-static u32 __get_uart_clk(void)
-{
-	u32 freq = 0, reg, pred, podf;
-	reg = __REG(MXC_CCM_CSCMR1);
-	switch ((reg & MXC_CCM_CSCMR1_UART_CLK_SEL_MASK) >>
-		MXC_CCM_CSCMR1_UART_CLK_SEL_OFFSET) {
-	case 0x0:
-		freq = __decode_pll(PLL1_CLK, CONFIG_MX51_HCLK_FREQ);
-		break;
-	case 0x1:
-		freq = __decode_pll(PLL2_CLK, CONFIG_MX51_HCLK_FREQ);
-		break;
-	case 0x2:
-		freq = __decode_pll(PLL3_CLK, CONFIG_MX51_HCLK_FREQ);
-		break;
-	case 0x3:
-		freq = __get_lp_apm();
-		break;
-	default:
-		break;
-	}
-
-	reg = __REG(MXC_CCM_CSCDR1);
-
-	pred = (reg & MXC_CCM_CSCDR1_UART_CLK_PRED_MASK) >>
-		MXC_CCM_CSCDR1_UART_CLK_PRED_OFFSET;
-
-	podf = (reg & MXC_CCM_CSCDR1_UART_CLK_PODF_MASK) >>
-		MXC_CCM_CSCDR1_UART_CLK_PODF_OFFSET;
-	freq /= (pred + 1) * (podf + 1);
-
-	return freq;
-}
-
 
 static u32 calc_per_cscdr1_val(u32 per_clk)
 {
