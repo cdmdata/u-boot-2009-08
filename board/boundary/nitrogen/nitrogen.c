44f280d9ad4fc2e6102014332d0d032be2e65509
Troy Kisky <troy.kisky@boundarydevices.com>
2011-05-30 12:21:35 -0700

    nitrogen: increase SW3 to 1.275V

--- board/boundary/nitrogen/nitrogen.c
+++ board/boundary/nitrogen/nitrogen.c
@@ -645,7 +645,7 @@ static void setup_core_voltage_spi(void)
 
 		/* Setup 1V2_DIG1 (SW3) to 1.275 */
 		val = pmic_reg(slave, 26, 0, 0);
-		val = (val & (~0x1f)) | mV_TO_CODE(1250);
+		val = (val & (~0x1f)) | mV_TO_CODE(1275);
 		pmic_reg(slave, 26, val, 1);
 		udelay(50);
 		/* Raise the core frequency to 800MHz */
