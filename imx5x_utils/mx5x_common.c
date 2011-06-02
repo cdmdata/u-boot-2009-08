22ddd043e543583664e4e3b63483e22767ea834d
Troy Kisky <troy.kisky@boundarydevices.com>
2011-06-02 13:20:58 -0700

    mx53: vbuckcore to 1.225V, old rev 1.775V if memory_init failed
    add checks to power up ddr on old board

--- imx5x_utils/mx5x_common.c
+++ imx5x_utils/mx5x_common.c
@@ -219,13 +219,13 @@ int ram_test(unsigned *ram_base)
 	return 0;
 }
 
-const unsigned char reg_data[] = {
+unsigned char da9052_init_data[] = {
 //If LDO10 is turned off then DA9053 accesses will fail
 //And it can never be turned back on. Fixed on next board
 		0x3b, 0x6a,		/* on,  LDO10, tfp410(6a:3.3V) */
 		0x33, 0x4c,		/* on,  LDO2, 0.893V (4c:0.9V) */
 //		0x34, 0x7f		/* on,  LDO3, 3.3V (7f:3.3V) */
-		0x2e, 0x5c,		/* on,  VBUCKCORE, 1.2V if srev < 3, 1.812V (0x73:1.775V) if old rev of board*/
+		0x2e, 0x5d,		/* on,  VBUCKCORE, 1.225V  (0x73:1.775V) if old rev of board*/
 		0x2f, 0x61,		/* on,  VBUCK_PRO, 1.302V (61:1.325V) */
 		0x30, 0x62,		/* on,  VBUCKMEM, 1.805V (62:1.775V) */
 		0x3c, 0x7f,		/* go:core, pro, mem, LDO2, LDO3 */
@@ -239,25 +239,100 @@ const unsigned char reg_data[] = {
 //		0x39, 0x0c,		/* off, LDO8, camera db(4c:1.8V) */
 		0x3a, 0x1f,		/* off, LDO9, 2.8V camera(5f:2.75V) */
 		0x1a, 0x97,		/* gp11 input, usbotg id */
+		0x1b, 0x09,		/* gp12 input, no LDO9_en */
 };
+
+int i2c_write_byte(unsigned i2c_base, unsigned chip, unsigned reg, unsigned char val)
+{
+	unsigned char buf[4];
+	buf[0] = val;
+	return i2c_write(i2c_base, chip, reg, 1, buf, 1);
+}
+
+int i2c_write_array(unsigned i2c_base, unsigned chip, const unsigned char* p, int size)
+{
+	int ret;
+	int i;
+	for (i = 0; i < size; i += 2) {
+		ret = i2c_write_byte(i2c_base, chip, p[0], p[1]);
+		p += 2;
+		if (ret)
+			return ret;
+	}
+	return 0;
+}
+
+int i2c_read_byte(unsigned i2c_base, unsigned chip, unsigned reg)
+{
+	int ret;
+	unsigned char buf[4];
+	ret = i2c_read(i2c_base, chip, reg, 1, buf, 1);
+	if (ret)
+		return -1;
+	return buf[0];
+}
+
+const unsigned char da9052_boost_vbuckcore_data[] = {
+		0x2e, 0x73,		/* on,  VBUCKCORE (0x73:1.775V) , old rev of board*/
+		0x3c, 0x61,		/* go:core */
+		0x1b, 0x0a,		/* gp12 output, open drain, internal pullup, high */
+};
+
+//An event with be registered if there is a 1 to 0 transition on gp12
+const unsigned char da9052_gp12_event[] = {
+		0x1b, 0x0a,		/* gp12 output, open drain, internal pullup, high */
+		0x1b, 0x09,		/* gp12 input, no LDO9_en , active low */
+};
+
+int vbuckcore_boost(unsigned i2c_base, unsigned chip)
+{
+	int ret;
+	unsigned srev = *((int *)0x63f98024); /* silicon revision */
+	if (srev >= 3)
+		return -5;
+	ret = i2c_read_byte(i2c_base, chip, 0x2e);
+	if (ret < 0)
+		return ret;
+	if (ret == 0x73)
+		return 1;
+	ret = i2c_write_array(i2c_base, chip, da9052_gp12_event, sizeof(da9052_gp12_event));
+	if (ret)
+		return ret;
+	ret = i2c_read_byte(i2c_base, chip, 0x04);
+	if (ret < 0)
+		return ret;
+	my_printf("statusd=%x\n", ret);
+	if (!(ret & 0x10))
+		return -2;	/* gp12 is grounded (new rev), don't change voltage */
+	ret = i2c_write_array(i2c_base, chip, da9052_boost_vbuckcore_data, sizeof(da9052_boost_vbuckcore_data));
+	delayMicro(1000);
+	return ret;
+}
 
 int power_up_ddr(unsigned i2c_base, unsigned chip)
 {
 	int ret;
-	int i;
-	unsigned char buf[4];
 	unsigned srev = *((int *)0x63f98024); /* silicon revision */
-	
+
 	i2c_init(i2c_base, 400000);
-	for (i = 0; i < sizeof(reg_data); i += 2) {
-		unsigned reg = reg_data[i];
-		buf[0] = reg_data[i+1];
-		if ((reg != 0x2e) || (srev < 3)) {
-			ret = i2c_write(i2c_base, chip, reg, 1, buf, 1);
-			if (ret)
-				return ret;
+	if (srev < 3) {
+		ret = i2c_read_byte(i2c_base, chip, 0x2e);
+		if (ret > 0) {
+			int code = ret & 0x3f;
+			if ((code >= 0x30) && (code <= 0x36)) {
+				/* don't change vbuckcore, reg 0x2e */
+				int i;
+				for (i = 0; i < sizeof(da9052_init_data); i+=2) {
+					if (da9052_init_data[i] == 0x2e) {
+						da9052_init_data[i+1] = (unsigned char)ret;
+						break;
+					}
+				}
+			}
 		}
 	}
+	ret = i2c_write_array(i2c_base, chip, da9052_init_data, sizeof(da9052_init_data));
 	delayMicro(1000);
+//	vbuckcore_boost(i2c_base, chip);
 	return ret;
 }
