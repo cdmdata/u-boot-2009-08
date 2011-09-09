210d5249d0228b6821f118cf8858c2212191acf1
Troy Kisky <troy.kisky@boundarydevices.com>
2011-09-08 18:38:56 -0700

    nitrogen53_a: doesn't read from SD card at 3.0 V, return to 3.3 V, break 4 wire touch

--- imx5x_utils/mx5x_common.c
+++ imx5x_utils/mx5x_common.c
@@ -244,7 +244,7 @@ static unsigned char da9052_init_data[] = {
 //And it can never be turned back on. Fixed on next board
 		0x3b, 0x6a,		/* on,  LDO10, tfp410(6a:3.3V) */
 		0x33, 0x4c,		/* on,  LDO2, 0.893V (4c:0.9V) */
-		0x34, 0x73,		/* on,  LDO3, 3.0V (73:3.0V) default (7f:3.3v) */
+//		0x34, 0x73,		/* on,  LDO3, 3.0V (73:3.0V) default (7f:3.3v) */
 #ifdef CONFIG_BOOST_VBUCKCORE
 		/* Always boost Nitrogen A */
 		0x2e, 0x73,		/* on,  VBUCKCORE (0x73:1.775V) , Nitrogen53A*/
