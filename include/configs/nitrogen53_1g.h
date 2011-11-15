510d5ecf7536d0702c8f6d5d87f458d5cb8d630f
Troy Kisky <troy.kisky@boundarydevices.com>
2011-11-15 14:21:05 -0700

    nitrogen53_1g: add 1G memory option

---
+++ include/configs/nitrogen53_1g.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2010 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the MX53-EVK Freescale board.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __NITROGEN53A_H
+#define __NITROGEN53A_H
+
+#include <configs/nitrogen53.h>
+#define CONFIG_H5PS2G83AFR_S6	/* 1G total memory */
+#endif
