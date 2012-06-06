026bcadb27d6100dce25ea939ffa8493bd2cf773
Eric Nelson <eric.nelson@boundarydevices.com>
2012-06-06 15:03:37 -0700

    Nitrogen53a config: Add _rev2 configuration to match Linux

---
+++ include/configs/nitrogen53a_rev2.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2010 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the MX53-EVK Freescale board.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __NITROGEN53A_H
+#define __NITROGEN53A_H
+
+#include <configs/nitrogen53.h>
+#undef CONFIG_MACH_TYPE
+#define CONFIG_MACH_TYPE	MACH_TYPE_MX53_NITROGEN_A
+#undef CONFIG_BOARD_NAME
+#define CONFIG_BOARD_NAME	"MX53-Nitrogen_A"
+
+#undef CONFIG_UART_BASE_ADDR
+#define CONFIG_UART_BASE_ADDR		UART3_BASE_ADDR
+
+#undef CONFIG_EXTRA_ENV_SETTINGS
+#define	CONFIG_EXTRA_ENV_SETTINGS	\
+	"ethprime=FEC0\0"		\
+	"machid=c62\0"			\
+
+#define CONFIG_POWER_KEY
+
+#define CONFIG_TFP410_HUB_EN	MAKE_GP(3, 11)
+#undef N53_I2C_CONNECTOR_BUFFER_ENABLE
+
+#undef CONFIG_PWM2_DUTY
+#define CONFIG_PWM2_DUTY	235	/* 235 out of 256 */
+
+#endif
